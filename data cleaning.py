3967d850a02d7e2e2713ccaf9739f79f52444b71
oguz toragay <ozt0008@auburn.edu>
2018-11-26 14:50:09 -0600

    Add files via upload

--- data cleaning.py
+++ data cleaning.py
@@ -1,19 +1,45 @@
 import numpy as np
 import pandas as pd
 import glob
-path =r'D:\Auburn University\6 - Fall 2018\1- INSY 6500 Information Technology for Operations\Homeworks\Project\data'
+path =r'C:\Users\oguzt\Desktop\to be done'
 allFiles = glob.glob(path + "/*.csv")
-list_ = []
-for file_ in allFiles:
-    df = pd.read_csv(file_,index_col = None, header = 0)
-    list_.append(df)
-frame = pd.concat(list_, axis = 0, ignore_index = True)
-
-
-k_ = frame.OFFICE
-for k in k_:
-    if k is str:
-        print(1)
+list = [pd.read_csv(file,index_col = None, header = 0) for file in allFiles] 
+#df = pd.concat(list, axis = 0, ignore_index = True)
+#####################################################################
+def convert_to_float(row):
+    string = str(row["AMOUNT"]).replace(",", "")
+    return float(string)
+#####################################################################
+def split_strings(row):
+    string = row["PAYEE"].rsplit( "  ", maxsplit=3)
+    return string[-1]
+#df.dropna()
+#f.dropna(subset=['PAYEE'], inplace=True)
+#####################################################################
+def split_strings2(row):
+    string2 = row["OFFICE"]
+    if string2[0:3]=='HON':
+        ret = string2[5:]
     else:
-        print(2)
-        
+        ret = string2
+    return ret  
+#####################################################################
+def year_finder(row):
+    string = row.QUARTER[0:4]
+    return int(string)
+#####################################################################
+df = list[1]
+df = df.loc[df["START DATE"] != '   ']
+df['START DATE'].replace('', np.nan, inplace=True)
+df.dropna(subset=['START DATE'], inplace=True)
+df["START DATE"] = pd.to_datetime(df["START DATE"])
+df = df.loc[df["END DATE"] != '   ']
+df['END DATE'].replace('', np.nan, inplace=True)
+df.dropna(subset=['END DATE'], inplace=True)
+df["END DATE"] = pd.to_datetime(df["END DATE"])
+df["AMOUNT"] = df.apply(lambda row: convert_to_float(row), axis =1)
+df["PAYEE"] = df.apply(lambda row: split_strings(row), axis= 1)
+df["YEAR"] = df.apply(lambda row: year_finder(row), axis= 1)
+df["OFFICE"] = df.apply(lambda row: split_strings2(row), axis=1)
+df.to_csv('2010 Q2.csv')
+#####################################################################
