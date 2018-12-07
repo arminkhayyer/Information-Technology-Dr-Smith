e3b763109f15644a22f8a3053dda5d8414b78174
arminkhayyer <armin.khayyer@gmail.com>
2018-12-06 23:26:06 -0600

    final

--- data in R.R
+++
@@ -1,6 +0,0 @@
-MyData <- read.csv(file="C:/Users/oguzt/Documents/GitHub/Information-Technology-Dr-Smith/ALLDATA.csv", header=TRUE, sep=",")
-head(MyData, n = 20, addrownums = FALSE)
-tail(MyData, n = 20, addrownums = FALSE)
-agg = aggregate(MyData$AMOUNT, by = list(MyData$YEAR),FUN = sum)
-agg
-sum(MyData$AMOUNT)
